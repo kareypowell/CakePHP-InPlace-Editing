53e6e557c6e0474cb72282945117dac6590a2da9
emilushi <emilushi@gmail.com>
2019-04-23 15:42:54 +0200

    Adding support for cakephp3

---
+++ src/View/Helper/InPlaceEditingHelper.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace InPlaceEditing\View\Helper;
+
+use Cake\View\Helper;
+
+/*
+ * ----------------------------------------------------------------------------
+ * Package:     CakePHP InPlaceEditing Plugin
+ * Version:     0.0.1
+ * Date:        2012-12-31
+ * Description: CakePHP plugin for in-place-editing functionality of any
+ *				form element.
+ * Author:      Karey H. Powell
+ * Author URL:  http://kareypowell.com/
+ * Repository:  http://github.com/kareypowell/CakePHP-InPlace-Editing
+ * ----------------------------------------------------------------------------
+ * Copyright (c) 2012 Karey H. Powell
+ * Dual licensed under the MIT and GPL licenses.
+ * ----------------------------------------------------------------------------
+ */
+
+class InPlaceEditingHelper extends Helper
+{
+
+    /*
+     * Returns a script which contains a html element (type defined in a parameter) with the field contents.
+     * And includes a script required for the inplace update ajax request logic.
+     */
+    public function input($modelName, $fieldName, $id, $settings = null)
+    {
+        $value         = $this::extractSetting($settings, 'value', '');
+        $actionName    = $this::extractSetting($settings, 'actionName', 'inPlaceEditing');
+        $type          = $this::extractSetting($settings, 'type', 'textarea');
+        $cancelText    = $this::extractSetting($settings, 'cancelText', 'Cancel');
+        $submitText    = $this::extractSetting($settings, 'submitText', 'Save');
+        $toolTip       = $this::extractSetting($settings, 'toolTip', 'Click to edit.');
+        $containerType = $this::extractSetting($settings, 'containerType', 'div');
+
+        $script = "
+			<$containerType id='inplace_".$modelName."_".$fieldName."_".$id."'>$value</$containerType>
+			<script type=\"text/javascript\">
+				$(
+					function()
+					{
+						$('#inplace_".$modelName."_".$fieldName."_".$id."').editable
+						(
+							'../$actionName/$id',
+							{
+								name      : 'data[$modelName][$fieldName]',
+								type      : '$type',
+								cancel    : '$cancelText',
+								submit    : '$submitText',
+								tooltip   : '$toolTip'
+							}
+						);
+					}
+				);
+			</script>
+		";
+
+        return $script;
+    }
+
+    /*
+     * Extracts a setting under the provided key if possible, otherwise, returns a provided default value.
+     */
+    protected static function extractSetting($settings, $key, $defaultValue = '')
+    {
+        if ( ! $settings && empty($settings)) {
+            return $defaultValue;
+        }
+
+        if (isset($settings[$key])) {
+            return $settings[$key];
+        } else {
+            return $defaultValue;
+        }
+    }
+
+}
